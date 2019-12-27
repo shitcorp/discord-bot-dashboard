9bf7dc5e6be39c8518622f05e5b8a091da928025
Julian Yaman <julianyaman@posteo.eu>
2019-12-27 02:41:40 +0100

    feat(dashboard): new Dashboard design (by AdminLTE.io)
    huge commit in terms of file size and file amount
    
    hope to get rid of most of them ... :/

--- modules/app.js
+++ modules/app.js
@@ -1,8 +1,45 @@
-const http = require('http')
+/***
+ * @fileOverview The back-end module which is running express.js
+ */
 
-exports.run = (port) => {
-  http.createServer((req, res) => {
-    res.write('Hello!')
-    res.end()
-  }).listen(port)
+const express = require('express')
+const app = express();
+
+// TODO: Check for security measure which need to be done before
+// TODO: express-session
+// TODO: better frontend lmao
+// TODO: Add routing system
+
+// Run
+exports.run = (client, port=3000) => {
+
+  const botInfo = {
+    username: client.user.username,
+    status: client.user.presence.status
+  }
+
+  /*
+  * App setup
+  */
+
+  // App view
+  app.set('view engine', 'pug')
+  app.set('views', './src/views')
+
+  // Asset directories
+  app.use('/static', express.static('./src/dist'))
+  app.use('/static', express.static('./src/plugins'))
+  /*
+  * Routing
+  */
+
+  // Index page
+  app.get('/', (req, res) => {
+    res.render('index', {botInfo: botInfo})
+  })
+
+  // Listener
+  app.listen(port, () => {
+    console.log('Application is running on port ' + port)
+  })
 }
