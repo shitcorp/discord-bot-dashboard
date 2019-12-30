71282cd024e65cc693a24a5a2c6ac67978ac0601
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2019-12-30 12:22:04 -0500

    feat(dashboard/login): adding a login system (#86)
    * Adding a login system

--- modules/app.js
+++ modules/app.js
@@ -1,13 +1,16 @@
 /***
  * @fileOverview The back-end module which is running express.js
  */
+const express = require('express');
+const passport = require("passport");
+const session = require('express-session');
 
-const express = require('express')
-const app = express()
+const DiscordStrategy = require("passport-discord.js").Strategy;
+const app = express();
 
 // Util modules
-const chalk = require('chalk')
-const log = console.log
+const chalk = require('chalk');
+const log = console.log;
 
 // TODO: Check for security measure which need to be done before
 // TODO: express-session
@@ -15,7 +18,49 @@ const log = console.log
 // TODO: Add routing system
 
 // Run
-exports.run = (client, port=3000) => {
+exports.run = (client, config) => {
+
+  /*
+* App setup
+*/
+
+  // App view
+  app.set('view engine', 'pug');
+  app.set('views', './src/views');
+
+  // Asset directories
+  app.use('/static', express.static('./src/dist'));
+  app.use('/static', express.static('./src/plugins'));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: false
+    }
+  }));
+
+  passport.use(new DiscordStrategy({
+        clientID: client.user.id,
+        clientSecret: config.clientSecret,
+        callbackURL: config.redirectURI,
+        scope: ["identify"]
+      },
+      function(accessToken, refreshToken, profile, done) {
+        //Handle Database Query Addition Here.
+        //console.log(profile);
+        return done(null, profile);
+      }
+  ));
+
+  passport.serializeUser(function(u, d) {
+    d(null, u);
+  });
+  passport.deserializeUser(function(u, d) {
+    d(null, u);
+  });
 
   // (only-read) Basic information about the bot
   const botInfo = {
@@ -23,30 +68,35 @@ exports.run = (client, port=3000) => {
     status: client.user.presence.status,
     users: client.users.size,
     guilds: client.guilds.size
-  }
+  };
 
-  /*
-  * App setup
-  */
-
-  // App view
-  app.set('view engine', 'pug')
-  app.set('views', './src/views')
-
-  // Asset directories
-  app.use('/static', express.static('./src/dist'))
-  app.use('/static', express.static('./src/plugins'))
   /*
   * Routing
   */
-
   // Index page
   app.get('/', (req, res) => {
-    res.render('index', {botInfo: botInfo})
-  })
+    if (!req.session.user) {
+      res.redirect('/auth/discord');
+    } else {
+      res.render('index', {botInfo: botInfo, userInfo: req.session.user});
+      //res.send(`Hello ${req.session.user.username}`);
+    }
+  });
+
+  app.get("/auth/discord", passport.authenticate("discord.js"));
+  app.get("/auth/discord/callback", passport.authenticate("discord.js", { failureRedirect: "/auth/discord/error" }), function(req, res) {
+    //console.log(req.session.passport);
+    req.session.user = req.session.passport.user;
+
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  });
+  app.get('/test', (req, res) => {
+    res.send(encodeURI(config.redirectURI));
+  });
 
   // Listener
-  app.listen(port, () => {
-    log('INFO >> ' + chalk.green('Dashboard is running on port ' + port))
-  })
+  app.listen(config.port, () => {
+    log('INFO >> ' + chalk.green('Dashboard is running on port ' + config.port));
+  });
 }
--- test.js
+++ test.js
@@ -4,19 +4,22 @@
  */
 const dashboard = require("./index");
 const Discord = require('discord.js');
-const chalk = require('chalk')
-require('dotenv').config() // Create a .env file or include your own config file
+const chalk = require('chalk');
+require('dotenv').config(); // Create a .env file or include your own config file
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
+const oAuth = Discord.OAuth2Application;
+
+const { OAuth2Application } = require('discord.js');
 
 // Ready event of the Client
 client.on('ready', () => {
+  
+  dashboard.run(client, { port: 4000, clientSecret: process.env.clientSecret, redirectURI: "http://localhost:4000/auth/discord/callback"}, oAuth);
+  console.log('INFO >> ' + chalk.green('Bot is online'));
 
-  dashboard.run(client, { port: 4000 })
-  console.log('INFO >> ' + chalk.green('Bot is online'))
-
-})
+});
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(process.env.TOKEN);
