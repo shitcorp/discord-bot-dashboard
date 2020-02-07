a972b88f5ef4c0679afe83d8ab6213ce6aaeb670
fydne <54745523+fydne@users.noreply.github.com>
2020-02-07 16:07:44 +0500

    ⠀

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('./modules/app')
+
+module.exports.run = (client, config, oAuth) => {
+
+    config = {
+        port: config.port || 443,
+        maintenanceNotification: config.maintenanceNotification || false,
+
+        baseGame: config.baseGame || "!help | v0.0.6.3",
+        baseBot_status: config.baseBot_status || "online",
+
+        maintenanceGame: config.maintenanceGame || "Bot is in maintenance",
+        maintenanceBot_status: config.maintenanceBot_status || "dnd",
+
+        clientSecret: oAuth.secret || config.clientSecret,
+        redirectURI: config.redirectURI || "https://dashboard.fydne.xyz/auth/discord/callback",
+    };
+
+    // Required: Discord.client, port (default: 3000)
+    app.run(client, config)
+
+}
